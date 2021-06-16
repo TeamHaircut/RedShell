f774cf3fef0fbebd5ad21c413a29ca3773d646dc
TeamHaircut <teamhaircutdiginet@gmail.com>
2021-06-16 17:28:45 -0400

    add servo.py

---
+++ servo.py
@@ -0,0 +1,12 @@
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(17)
+servo.min( )
+while True:
+	print("True")
+	servo.min( )
+	sleep(1.5)
+	servo.mid( )
+	sleep(1.5)
+	servo.max( )
