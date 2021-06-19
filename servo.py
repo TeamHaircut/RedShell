8eff5d2b3d1e48f4cc088b33894d0f6e83367e4f
TeamHaircut <teamhaircutdiginet@gmail.com>
2021-06-19 12:41:12 -0400

    update servo

--- servo.py
+++ servo.py
@@ -1,46 +1,19 @@
+from evdev import InputDevice, list_devices, ecodes
 from gpiozero import Servo
-import time 
-import curses
-
-# get the curses screen window
-screen = curses.initscr()
-
-# turn off input echoing
-curses.noecho()
-
-# respond to keys immediately (don't wait for enter)
-curses.cbreak()
-
-# map arrow keys to special values
-screen.keypad(True)
+import time
 
 servo = Servo(17)
 servo.mid()
 
-try:
-	while True:
-		print(servo.value)
-		char = screen.getch()
-		if char == ord('q'): 
-			break
-		if char == curses.KEY_RIGHT:
-			if servo.value != 1.0:
-				print("Right Key pressed")
-				servo.max()
-				time.sleep(1)
-			
-		if char == curses.KEY_LEFT:
-			if servo.value != -1.0:
-				print("Left Key pressed")
-				servo.min()
-				time.sleep(1)
+# create object gamepad
+gamepad = InputDevice('/dev/input/event0')
 
-except KeyboardInterrupt:
-	print("Program Terminated")
-			
-finally:
-	# shut down cleanly
-	curses.nocbreak()
-	screen.keypad(False)
-	curses.echo()
-	curses.endwin()
+for event in gamepad.read_loop():
+	if event.type == ecodes.EV_ABS:
+		#print(event.value)
+		if event.value == 255:
+			print("Right")
+			servo.max()
+		if event.value == 0:
+			print("Left")
+			servo.min()
