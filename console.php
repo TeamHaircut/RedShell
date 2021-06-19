6040c668f30b8000f6533d0865061d5647741a6b
TeamHaircut <teamhaircutdiginet@gmail.com>
2021-06-19 17:04:39 -0400

    add php

--- console.php
+++ console.php
@@ -1,25 +1,22 @@
-<!DOCTYPE html>
-<!--TheFreeElectron 2015, http://www.instructables.com/member/TheFreeElectron/ -->
-
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Test</title>
+        <title>Raspberry Pi Gpio</title>
     </head>
  
-    <body style="background-color: black;">
+    <body style="background-color: white;">
     <!-- On/Off button's picture -->
 	<?php
-	$val_array = array(0,0,0,0);
+	$val_array = array(0,0);
 	//this php script generate the first page in function of the file
-	for ( $i= 0; $i<4; $i++) {
+	for ( $i= 0; $i<2; $i++) {
 		//set the pin's mode to output and read them
 		system("gpio mode ".$i." out");
 		exec ("gpio read ".$i, $val_array[$i], $return );
 	}
 	//for loop to read the value
 	$i =0;
-	for ($i = 0; $i < 4; $i++) {
+	for ($i = 0; $i < 2; $i++) {
 		//if off
 		if ($val_array[$i][0] == 0 ) {
 			echo ("<img id='button_".$i."' src='con/red_".$i.".png' onclick='change_pin (".$i.");'/>");
@@ -35,4 +32,3 @@
 	<script src="script.js"></script>
     </body>
 </html>
-
