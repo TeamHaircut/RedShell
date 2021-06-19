d60288b6711f7ee663430fa29c97e7e6f52b92f4
TeamHaircut <teamhaircutdiginet@gmail.com>
2021-06-19 11:04:06 -0400

    update servo

--- servo.py
+++ servo.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+from gpiozero import Servo
 import time 
 import curses
 
@@ -14,33 +14,33 @@ curses.cbreak()
 # map arrow keys to special values
 screen.keypad(True)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
+servo = Servo(17)
+servo.mid()
 
-p = GPIO.PWM(17,50)
-p.start(7.50)
-angle = 0.3
-my = 7.5 
 try:
 	while True:
+		print(servo.value)
 		char = screen.getch()
 		if char == ord('q'): 
 			break
 		if char == curses.KEY_RIGHT:
-			print("Right Key pressed")
-			my += 1.0
-			p.ChangeDutyCycle(my)
-			time.sleep(1)
+			if servo.value != 1.0:
+				print("Right Key pressed")
+				servo.max()
+				time.sleep(1)
 			
 		if char == curses.KEY_LEFT:
-			print("Left Key pressed")
-			my -= 1.0
-			p.ChangeDutyCycle(12.5)
+			if servo.value != -1.0:
+				print("Left Key pressed")
+				servo.min()
+				time.sleep(1)
+
+except KeyboardInterrupt:
+	print("Program Terminated")
 			
 finally:
-    # shut down cleanly
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
-		
-    p.stop()	
-    GPIO.cleanup()
+	# shut down cleanly
+	curses.nocbreak()
+	screen.keypad(False)
+	curses.echo()
+	curses.endwin()
