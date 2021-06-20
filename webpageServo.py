1bd3910c97ef28596ac80eac3affbbf5a0a9b04c
TeamHaircut <teamhaircutdiginet@gmail.com>
2021-06-19 21:47:16 -0400

    update webpageServo

--- webpageServo.py
+++ webpageServo.py
@@ -14,12 +14,39 @@ TPL = '''
 <html>
     <head><title>Web Page Controlled Servo</title></head>
     <body>
-        <form method="POST" action="left">
-            <input type="submit" value="left" />
-        </form>
-		<form method="POST" action="right">
-            <input type="submit" value="right" />
-        </form>
+		<form method="POST" action="left" id="leftbtn">
+		</form>
+		<form method="POST" action="right" id="rightbtn">
+		</form>
+
+		<script>
+		window.addEventListener('gamepadconnected', (event) => {
+		console.log('A gamepad was connected:', event.gamepad);
+
+		});
+		function updateGamepad() {
+			requestAnimationFrame(updateGamepad);
+
+			// We'll only get the first gamepad in our list.
+			const gamepad = navigator.getGamepads()[0];
+
+			// If our gamepad isn't connected, stop here.
+			if (!gamepad) return;
+
+        		if (gamepad.axes[0] == -1)
+        		{
+					document.getElementById('leftbtn').submit();
+        		}
+
+				if (gamepad.axes[0] == 1)
+        		{
+					document.getElementById('rightbtn').submit();
+        		}
+		}
+
+		updateGamepad();
+
+		</script>
     </body>
 </html>
 
@@ -29,16 +56,18 @@ def home():
     return render_template_string(TPL)                        
 @app.route("/left", methods=["POST"])
 def left():
-    # Change duty cycle
+    print("left")
     p.ChangeDutyCycle(1.0)
+    # Change duty cycle
     sleep(1)
     # Pause the servo
     p.ChangeDutyCycle(0)
     return render_template_string(TPL)
 @app.route("/right", methods=["POST"])
 def right():
-    # Change duty cycle
+    print("Right")
     p.ChangeDutyCycle(12.5)
+    # Change duty cycle
     sleep(1)
     # Pause the servo
     p.ChangeDutyCycle(0)
